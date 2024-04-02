4aa9caf8ca4b752b3cb66c9c31b9ed5de15f71eb
Italia Maria Desiré Cardullo <cardullo.lia@gmail.com>
2024-04-02 09:35:04 +0200

    feat: setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
+import Home from './pages/home';
 
 const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Codeploy Randomizer
-      </h1>
-      <p>React TS + TailwindCSS</p>
+      <Home/>
     </>
   )
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        
+      </h1>
+      <p>React TS + TailwindCSS</p>
+    </>
+  )
+}
+
+export default Home;
